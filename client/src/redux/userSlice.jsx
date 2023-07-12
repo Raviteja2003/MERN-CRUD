75f045bfda56d2c6409f3a09b07daf1b870659bf
Raviteja2003 <ravitejat0406@gmail.com>
2023-07-12 15:23:10 +0530

    initial commit

---
+++ client/src/redux/userSlice.jsx
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice(
+    {
+        name:"users",
+        initialState:{
+            users:[]
+        },
+        reducers:{
+
+        }
+    }
+)
+
+export const {} = userSlice.actions;
+export default userSlice.reducers;
